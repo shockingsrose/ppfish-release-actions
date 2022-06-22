9dfeb357b7f127ac0d265c7702f54e06e918adb7
storm <847073376@qq.com>
2022-06-22 15:37:07 +0800

    feat: 文案调整

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Github Workflows</p>
+        <p>Hello Github Workflows 1.0.3</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
